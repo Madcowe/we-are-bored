933853f55dec0f3c5f96532378df9faa4aba4310
Madcowe <34983336+Madcowe@users.noreply.github.com>
2025-08-26 18:25:28 +0100

    Version no updated

--- surf-bored/src/directory.rs
+++ surf-bored/src/directory.rs
@@ -125,7 +125,7 @@ impl Directory {
 pub fn about_bored() -> Bored {
     let mut about = Bored::create("About", Coordinate { x: 80, y: 30 });
     let mut notice = Notice::create(Coordinate { x: 20, y: 5 });
-    notice.write("Surf Bored\n\nV0.3.3").unwrap();
+    notice.write("Surf Bored\n\nV0.4.1").unwrap();
     about.add(notice, Coordinate { x: 3, y: 2 }).unwrap();
     let mut notice = Notice::create(Coordinate { x: 50, y: 5 });
     notice
